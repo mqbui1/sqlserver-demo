63a2341c34180e7da0f4f32dbd66f495fff46880
Marc Bui <mqbui1@gmail.com>
2026-01-20 22:55:54 -0800

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.service.DbLatencyService;
+import com.example.demo.DbLatencyService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
