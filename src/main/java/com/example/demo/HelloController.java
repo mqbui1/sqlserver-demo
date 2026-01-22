fe02205666c2bfb103ba3ac0e06b67fa4e42adf8
Marc Bui <mqbui1@gmail.com>
2026-01-21 20:57:46 -0800

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import com.example.demo.DbLatencyService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +14,7 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
-    public String hello(
-            @RequestParam(name = "delay", required = false) Integer delaySeconds
-    ) {
-        return dbLatencyService.getGreeting(delaySeconds);
+    public String hello() {
+        return dbLatencyService.getGreetingWithDbLatency();
     }
 }
