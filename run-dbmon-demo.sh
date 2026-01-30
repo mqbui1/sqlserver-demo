12cafa4bdb883dd4dba2c88cc71bb04847264489
Marc Bui <mqbui1@gmail.com>
2026-01-29 23:41:35 -0800

    Update and rename run-aiagent-demo.sh to run-dbmon-demo.sh

---
+++ run-dbmon-demo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+########################
+# include the magic
+########################
+. demo-magic.sh
+
+# hide the evidence
+clear
+
+# Put your stuff here
+pe ". check_env.sh"
+
+pe "docker buildx create --use"
+
+pe "docker buildx build \
+  --platform linux/amd64 \
+  -t mqbui1/sqlserver-demo:latest \
+  --push \
+  ."
+
+pe "kubectl apply -f k8s/"
+
+pe "helm install splunk-otel-collector \
+--set="operatorcrds.install=true", \
+--set="operator.enabled=true", \
+--set="splunkObservability.realm=$REALM" \
+--set="splunkObservability.accessToken=$ACCESS_TOKEN" \
+--set="clusterName=$INSTANCE-k3s-cluster" \
+--set="splunkObservability.profilingEnabled=true" \
+--set="agent.discovery.enabled=true" \
+--set="agent.service.enabled=true"  \
+--set="environment=$INSTANCE-workshop" \
+--set="splunkPlatform.endpoint=$HEC_URL" \
+--set="splunkPlatform.token=$HEC_TOKEN" \
+--set="splunkPlatform.index=splunk4rookies-workshop" \
+splunk-otel-collector-chart/splunk-otel-collector \
+-f otel/otel-collector.yaml"
